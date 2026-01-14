3cf6817fd3129bdf95632185c7273c745cfdbb42
sawdaicw <lzr061014@ruc.edu.cn>
2026-01-14 11:37:14 +0800

    Delete tools/dataset_Remote/falcon directory

--- tools/dataset_Remote/falcon/upload.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import HfApi
-api = HfApi()
-api.upload_folder(
-    folder_path="./falcon/Falcon-Single-Instruction-Large",
-    repo_id="juanjuanjuanbing/falcon",
-    repo_type="model"
-)
